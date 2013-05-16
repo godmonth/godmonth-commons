18efa309bc7fdc0a0cd4816042d41912d6ebb9ad
godmonth <link2people@gmail.com>
2013-05-16 18:20:52 +0800

    commit

--- src/main/java/com/godmonth/util/advices/lock/InterProcessMutexAdvice.java
+++ src/main/java/com/godmonth/util/advices/lock/InterProcessMutexAdvice.java
@@ -2,16 +2,22 @@ package com.godmonth.util.advices.lock;
 
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class InterProcessMutexAdvice {
 	private InterProcessMutex interProcessMutex;
 
+	private static final Logger logger = LoggerFactory.getLogger(InterProcessMutexAdvice.class);
+
 	public Object cool(ProceedingJoinPoint joinPoint) throws Throwable {
 		try {
+			logger.debug("acquiring lock");
 			interProcessMutex.acquire();
 			return joinPoint.proceed();
 		} finally {
 			interProcessMutex.release();
+			logger.debug("acquire released");
 		}
 	}
 
