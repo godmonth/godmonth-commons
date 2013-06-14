94fc765bfc8ccb6206dd3f5e17e742e8a5087b6b
godmonth <link2people@gmail.com>
2013-06-14 10:23:23 +0800

    commit

--- src/main/java/com/godmonth/util/advices/cooler/DailyCoolerAdvice.java
+++ src/main/java/com/godmonth/util/advices/cooler/DailyCoolerAdvice.java
@@ -3,7 +3,6 @@ package com.godmonth.util.advices.cooler;
 import java.util.Calendar;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 
 import org.apache.commons.lang3.time.DateUtils;
 import org.joda.time.DateTime;
@@ -11,7 +10,7 @@ import org.joda.time.DateTime;
 public class DailyCoolerAdvice extends CoolerAdvice {
 	private int wakenHour;
 
-	public boolean checkCool( DateTime end) {
+	public boolean checkCool(DateTime end) {
 		DateTime lastDate = lastExecution.getValue();
 		return end.getHourOfDay() >= wakenHour
 				&& (lastDate == null || DateUtils.truncatedCompareTo(end.toDate(), lastDate.toDate(), Calendar.DATE) > 0);
