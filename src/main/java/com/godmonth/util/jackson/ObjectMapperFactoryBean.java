b4d0f463d4270b4c1bb82029fdf29e65e7caef5e
godmonth <link2people@gmail.com>
2013-06-17 13:33:51 +0800

    commit

--- src/main/java/com/godmonth/util/jackson/ObjectMapperFactoryBean.java
+++ src/main/java/com/godmonth/util/jackson/ObjectMapperFactoryBean.java
@@ -1,61 +1,27 @@
 package com.godmonth.util.jackson;
 
-import org.springframework.beans.factory.FactoryBean;
-import org.springframework.beans.factory.InitializingBean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 /**
  * @author shenyue
  */
-public class ObjectMapperFactoryBean implements FactoryBean<ObjectMapper>, InitializingBean {
-	private ObjectMapper objectMapper;
+public class ObjectMapperFactoryBean extends Jackson2ObjectMapperFactoryBean {
 	private boolean camel;
-	private boolean singleton = true;
-	private boolean prettyPrint;
-	private boolean failOnUnkownProperties;
 
 	@Override
 	public void afterPropertiesSet() {
-		objectMapper = new ObjectMapper();
+		super.afterPropertiesSet();
 		if (camel) {
-			objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+			getObject().setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
 		}
-		objectMapper.configure(SerializationFeature.INDENT_OUTPUT, prettyPrint);
-		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, failOnUnkownProperties);
-		objectMapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
-		objectMapper.setSerializationInclusion(Include.NON_NULL);
-	}
-
-	@Override
-	public ObjectMapper getObject() throws Exception {
-		return objectMapper;
-	}
-
-	@Override
-	public Class<?> getObjectType() {
-		return ObjectMapper.class;
-	}
-
-	@Override
-	public boolean isSingleton() {
-		return singleton;
+		getObject().setSerializationInclusion(Include.NON_NULL);
 	}
 
 	public void setCamel(boolean camel) {
 		this.camel = camel;
-	}
-
-	public void setPrettyPrint(boolean prettyPrint) {
-		this.prettyPrint = prettyPrint;
-	}
-
-	public void setFailOnUnkownProperties(boolean failOnUnkownProperties) {
-		this.failOnUnkownProperties = failOnUnkownProperties;
 	}
 
 }
