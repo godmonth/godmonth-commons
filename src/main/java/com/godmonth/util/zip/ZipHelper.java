e3119b8b37f95bac4b75c6d683ad8a3a54e4d05a
godmonth <link2people@gmail.com>
2013-08-08 10:40:57 +0800

    add zip helper

---
+++ src/main/java/com/godmonth/util/zip/ZipHelper.java
@@ -0,0 +1,24 @@
+package com.godmonth.util.zip;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
+public class ZipHelper {
+
+	public static void unzip(String zipFile, String workingDir, boolean createSubDir) throws IOException, ZipException {
+		FileUtils.forceMkdir(new File(workingDir));
+		if (createSubDir) {
+			String baseName = FilenameUtils.getBaseName(zipFile);
+			workingDir += "/" + baseName;
+			FileUtils.forceMkdir(new File(workingDir));
+		}
+		ZipFile zf = new ZipFile(zipFile);
+		zf.extractAll(workingDir);
+	}
+}
