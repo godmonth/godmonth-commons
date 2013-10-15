109cc973cb2c651aea7e8e6dd7eeb766f408319a
godmonth <link2people@gmail.com>
2013-10-15 19:55:16 +0800

    commit

--- src/main/java/com/godmonth/util/jackson/ObjectMapperFactoryBean.java
+++ src/main/java/com/godmonth/util/jackson/ObjectMapperFactoryBean.java
@@ -1,5 +1,8 @@
 package com.godmonth.util.jackson;
 
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -10,18 +13,53 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
  */
 public class ObjectMapperFactoryBean extends Jackson2ObjectMapperFactoryBean {
 	private boolean camel;
+	private List<Mixin> mixins;
 
 	@Override
 	public void afterPropertiesSet() {
 		super.afterPropertiesSet();
 		if (camel) {
-			getObject().setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+			getObject()
+					.setPropertyNamingStrategy(
+							PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
 		}
 		getObject().setSerializationInclusion(Include.NON_NULL);
+		if (CollectionUtils.isNotEmpty(mixins)) {
+			for (Mixin mixin : mixins) {
+				getObject().addMixInAnnotations(mixin.getTarget(),
+						mixin.getMixinSource());
+			}
+		}
+
 	}
 
 	public void setCamel(boolean camel) {
 		this.camel = camel;
 	}
 
+	public void setMixins(List<Mixin> mixins) {
+		this.mixins = mixins;
+	}
+
+	public static class Mixin {
+		private Class<?> target;
+		private Class<?> mixinSource;
+
+		public Class<?> getTarget() {
+			return target;
+		}
+
+		public void setTarget(Class<?> target) {
+			this.target = target;
+		}
+
+		public Class<?> getMixinSource() {
+			return mixinSource;
+		}
+
+		public void setMixinSource(Class<?> mixinSource) {
+			this.mixinSource = mixinSource;
+		}
+
+	}
 }
