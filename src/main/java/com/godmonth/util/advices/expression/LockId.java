73c61e8048a1d94fa110af102f8425293dacf42a
godmonth <link2people@gmail.com>
2013-12-10 18:36:47 +0800

    commit

---
+++ src/main/java/com/godmonth/util/advices/expression/LockId.java
@@ -0,0 +1,20 @@
+package com.godmonth.util.advices.expression;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface LockId {
+	/**
+	 * 表达式
+	 * 
+	 * @return lockId的值
+	 */
+	String expression();
+
+}
