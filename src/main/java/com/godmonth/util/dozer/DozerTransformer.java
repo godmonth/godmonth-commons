a438dc3adb75f7162c41196f3ef5ed605ac54f8d
godmonth <link2people@gmail.com>
2013-12-16 13:08:38 +0800

    commit

--- src/main/java/com/godmonth/util/dozer/DozerTransformer.java
+++ src/main/java/com/godmonth/util/dozer/DozerTransformer.java
@@ -1,20 +1,30 @@
 package com.godmonth.util.dozer;
 
 import org.apache.commons.collections4.Transformer;
+import org.apache.commons.lang3.StringUtils;
 import org.dozer.Mapper;
 
 public class DozerTransformer<OUT> implements Transformer<Object, OUT> {
 	private Mapper mapper;
 	private Class<OUT> clazz;
+	private final String mapId;
 
 	public DozerTransformer(Mapper mapper, Class<OUT> clazz) {
-		this.mapper = mapper;
-		this.clazz = clazz;
+		this(mapper, clazz, null);
 	}
 
-	@Override
+	public DozerTransformer(Mapper mapper, Class<OUT> clazz, String mapId) {
+		this.mapper = mapper;
+		this.clazz = clazz;
+		this.mapId = mapId;
+	}
+
 	public OUT transform(Object input) {
-		return mapper.map(input, clazz);
+		if (StringUtils.isNotBlank(mapId)) {
+			return mapper.map(input, clazz, mapId);
+		} else {
+			return mapper.map(input, clazz);
+		}
 	}
 
 }
