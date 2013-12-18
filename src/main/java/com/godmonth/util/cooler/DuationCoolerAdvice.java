33b70e3a923f661b51610e6bf2a3d4406a78a33f
godmonth <link2people@gmail.com>
2013-12-18 19:27:36 +0800

    commit

--- src/main/java/com/godmonth/util/cooler/DuationCoolerAdvice.java
+++ src/main/java/com/godmonth/util/cooler/DuationCoolerAdvice.java
@@ -2,14 +2,20 @@ package com.godmonth.util.cooler;
 
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
 
 public class DuationCoolerAdvice extends CoolerAdvice {
+
+	private static final Logger logger = LoggerFactory.getLogger(DuationCoolerAdvice.class);
+
 	private Duration leastDuation;
 
 	@Override
 	public boolean checkCool(DateTime end) {
 		DateTime lastDate = lastExecution.getValue();
+		logger.trace("lastExecution:{}", lastExecution);
 		return lastDate == null || new Duration(lastDate, end).compareTo(leastDuation) > 0;
 	}
 
