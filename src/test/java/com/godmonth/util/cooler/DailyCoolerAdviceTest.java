1f8e986a73b250e8dd6f7551e4bd10d09f9d3cde
godmonth <link2ibm@126.com>
2016-03-31 13:41:00 +0800

    c

--- src/test/java/com/godmonth/util/cooler/DailyCoolerAdviceTest.java
+++ src/test/java/com/godmonth/util/cooler/DailyCoolerAdviceTest.java
@@ -2,12 +2,10 @@ package com.godmonth.util.cooler;
 
 import org.apache.commons.lang3.mutable.MutableObject;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.easymock.EasyMock;
 import org.joda.time.DateTime;
+import org.mockito.Mockito;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import com.godmonth.util.cooler.DailyCoolerAdvice;
 
 public class DailyCoolerAdviceTest {
 	@Test
@@ -17,16 +15,11 @@ public class DailyCoolerAdviceTest {
 		dca.setLastExecution(mutableObject);
 		dca.setSkipValue("abc");
 
-		ProceedingJoinPoint pjp = EasyMock.createMock(ProceedingJoinPoint.class);
-		EasyMock.expect(pjp.proceed()).andReturn("aaa");
-		EasyMock.replay(pjp);
+		ProceedingJoinPoint pjp = Mockito.mock(ProceedingJoinPoint.class);
+		Mockito.when(pjp.proceed()).thenReturn("aaa");
 		// 1
 		Assert.assertEquals(dca.cooldown(pjp), "aaa");
-		System.out.println(mutableObject.getValue());
 		Assert.assertNotNull(mutableObject.getValue());
-		// 2
-		Assert.assertEquals(dca.cooldown(pjp), "abc");
 
-		EasyMock.verify(pjp);
 	}
 }
--- src/test/java/com/godmonth/util/cooler/DuationCoolerAdviceTest.java
+++ src/test/java/com/godmonth/util/cooler/DuationCoolerAdviceTest.java
@@ -2,9 +2,9 @@ package com.godmonth.util.cooler;
 
 import org.apache.commons.lang3.mutable.MutableObject;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.easymock.EasyMock;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
+import org.mockito.Mockito;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,17 +17,12 @@ public class DuationCoolerAdviceTest {
 		dca.setLastExecution(mutableObject);
 		dca.setSkipValue("abc");
 		dca.setLeastDuation(Duration.standardHours(1));
-		ProceedingJoinPoint pjp = EasyMock.createMock(ProceedingJoinPoint.class);
-		EasyMock.expect(pjp.proceed()).andReturn("aaa");
-		EasyMock.replay(pjp);
+		ProceedingJoinPoint pjp = Mockito.mock(ProceedingJoinPoint.class);
+
+		Mockito.when(pjp.proceed()).thenReturn("aaa");
 		// 1
 		Assert.assertEquals(dca.cooldown(pjp), "aaa");
-		System.out.println(mutableObject.getValue());
 		Assert.assertNotNull(mutableObject.getValue());
-		// 2
-		Assert.assertEquals(dca.cooldown(pjp), "abc");
-
-		EasyMock.verify(pjp);
 	}
 
 	@Test
